0d7b18ce09b48708e51233f8b04bb735e84a5cb9
akhil <akhilmaheeps12@gmail.com>
2020-07-08 13:50:43 -0400

    Cleaned 10-ks

--- filingsData.py
+++ filingsData.py
@@ -6,6 +6,11 @@ import pprint
 import project_helper
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import re
+from nltk.corpus import wordnet, stopwords
+from nltk.stem import  WordNetLemmatizer
+from collections import Counter, defaultdict
+from sklearn.feature_extraction.text import CountVectorizer
 
 cik_lookup = {
     'AMZN': '0001018724',
@@ -54,8 +59,99 @@ for ticker, data in sec_data.items():
             raw_filings_by_ticker[ticker][file_date] = sec_api.get(file_url)
 
 
-print('Example Document: \n\n{}...'.format(next(iter(raw_filings_by_ticker[test_ticker].values()))[:1000]))
+#print('Example Document: \n\n{}...'.format(next(iter(raw_filings_by_ticker[test_ticker].values()))[:1000]))
 
+
+def get_documents(text):
+    extracted_docs = []
+    doc_start_pattern = re.compile(r"<DOCUMENT>")
+    doc_end_pattern = re.compile(r"</DOCUMENT>")
+    
+    doc_start_is = [m.end() for m in doc_start_pattern.finditer(text)]
+    doc_end_is  = [m.start() for m in doc_end_pattern.finditer(text)]
+
+    for doc_start_i, doc_end_i in zip(doc_start_is, doc_end_is):
+        extracted_docs.append(text[doc_start_i:doc_end_i])
+
+    return extracted_docs
+
+filling_documents_by_ticker = {}
+for ticker,raw_fillings in raw_filings_by_ticker.items():
+    filling_documents_by_ticker[ticker] = {}
+    for file_date, filling in tqdm(raw_fillings.items(), desc = 'Getting documents from {} Filings'.format(ticker), unit = 'filling'):
+        filling_documents_by_ticker[ticker][file_date] = get_documents(filling)
+
+#print('\n\n'.join(['Document {} Filed on {}: \n{}...'.format(doc_i,file_date,doc[:200])
+      #for file_date, docs in filling_documents_by_ticker[test_ticker].items()
+      #for doc_i, doc in enumerate(docs)][:3]))
+
+
+def get_document_type(doc):
+    type_pattern = re.compile(r'<TYPE>[^\n]+')
+    doc_type = type_pattern.findall(doc)[0][len('<TYPE>'):]
+
+    return doc_type.lower()
+
+ten_ks_by_ticker = {}
+for ticker, filling_documents in filling_documents_by_ticker.items():
+    ten_ks_by_ticker[ticker] = []
+    for file_date, documents in filling_documents.items():
+        for document in documents:
+            if get_document_type(document) == '10-k':
+                ten_ks_by_ticker[ticker].append({
+                    'cik': cik_lookup[ticker],
+                    'file': document,
+                    'file_date': file_date
+                })
+
+#project_helper.print_ten_k_data(ten_ks_by_ticker[test_ticker][:5], ['cik','file','file_date'])
+
+def remove_html_tags(text):
+    text = BeautifulSoup(text, 'html.parser').get_text()
+    return text
+
+def clean_text(text):
+    text = text.lower()
+    text = remove_html_tags(text)
+    return text
+
+
+for ticker, ten_ks in ten_ks_by_ticker.items():
+    for ten_k in tqdm(ten_ks,desc="Cleaning {} 10-Ks".format(ticker), unit="10-K"):
+        ten_k['file clean'] = clean_text(ten_k['file'])
+
+#project_helper.print_ten_k_data(ten_ks_by_ticker[test_ticker][:5], ['file clean'])
+
+def lemmatize_words(words):
+    lemmatized_words = [WordNetLemmatizer().lemmatize(word,'v') for word in words]
+    return lemmatized_words
+
+word_pattern = re.compile(r'\w+')
+
+for ticker, ten_ks in ten_ks_by_ticker.items():
+    for ten_k in tqdm(ten_ks, desc="Lemmatize {} 10-ks".format(ticker), unit='10-K'):
+        ten_k['file_lemma'] = lemmatize_words(word_pattern.findall(ten_k['file clean']))
+
+#project_helper.print_ten_k_data(ten_ks_by_ticker[test_ticker][:5],['file_lemma'])
+
+lemma_english_stopwords =  lemmatize_words(stopwords.words('english'))
+for ticker, ten_ks in ten_ks_by_ticker.items():
+    for ten_k in tqdm(ten_ks,desc='Remove Stop Words for {} 10-Ks'.format(ticker), unit='10-K'):
+        ten_k['file_lemma'] = [word for word in ten_k['file_lemma'] if word not in lemma_english_stopwords]
+
+print('Stopwords removed')
+
+#Loughran-McDonald sentiment word lists for analysis
+
+sentiments = ['positive','negative','uncertainty','litigious','constraining','interesting']
+sentiment_df = pd.read_csv('LoughranMcDonald_MasterDictionary_2018.csv')
+sentiment_df.columns = [column.lower() for column in sentiment_df.columns]
+
+#Removing unused information
+
+sentiment_df = sentiment_df[sentiments + ['word']]
+sentiment_df[sentiments] = sentiment_df[sentiments].astype(bool)
+sentiment_df = sentiment_df[(sentiment_df[sentiments].any(1))]#check
 
 
 
